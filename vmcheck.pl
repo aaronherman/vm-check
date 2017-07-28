c66f8c7f9c2a7fe5f13bb849e64c5bb3b75e9559
Aaron Herman <aaron@aaronherman.co>
2017-07-28 11:56:31 -0500

    add better structure and input logic

--- vmcheck.pl
+++ vmcheck.pl
@@ -9,61 +9,77 @@ use Term::ANSIColor;
 use DBI;
 my $dbh = DBI->connect('dbi:WMI:');
 
-
-print "\n====================\n";
-print " System Information\n";
-print "====================\n\n";
-
-
-my $os_name = Win32::GetOSDisplayName();
-print "OS Name: $os_name\n";
-
-my $admin = Win32::IsAdminUser();
-print "Admin running script: ", $admin ? "Yes" : "No","\n";
+sub main {
+  print "\n====================\n";
+  print " System Information\n";
+  print "====================\n\n";
 
 
-my $name = Win32::LoginName();
-print "Login name: $name\n";
+  my $os_name = Win32::GetOSDisplayName();
+  print "OS Name: $os_name\n";
 
-$host = hostname;
-print "Host Name: $host\n";
+  my $admin = Win32::IsAdminUser();
+  print "Admin running script: ", $admin ? "Yes" : "No","\n";
 
 
-#Default printer
-my $printer_query = $dbh->prepare(<<WQL);
-  SELECT * FROM Win32_Printer WHERE Default = TRUE
+  my $name = Win32::LoginName();
+  print "Login name: $name\n";
+
+  $host = hostname;
+  print "Host Name: $host\n";
+
+
+  #Default printer
+  my $printer_query = $dbh->prepare(<<WQL);
+    SELECT * FROM Win32_Printer WHERE Default = TRUE
 WQL
 
-$printer_query->execute();
-while (my @row = $printer_query->fetchrow) {
-  print "Default Printer: ", $row[0]->{Caption}, "\n";
-}
+  $printer_query->execute();
+  while (my @row = $printer_query->fetchrow) {
+    print "Default Printer: ", $row[0]->{Caption}, "\n";
+  }
 
 
-print "\n======================\n";
-print " Hardware Information\n";
-print "======================\n\n";
+  print "\n======================\n";
+  print " Hardware Information\n";
+  print "======================\n\n";
 
 
-my %memory;
-Win32::SystemInfo::MemoryStatus(%memory,'GB');
-print "RAM: $memory{TotalPhys} Gb\n";
+  my %memory;
+  Win32::SystemInfo::MemoryStatus(%memory,'GB');
+  print "RAM: $memory{TotalPhys} Gb\n";
 
-if (($os_name =~ /(([^64-bit]+))/) && ($memory{TotalPhys} < 3.8)) {
-  print color("red"), "64-bit OS with less than 3.8Gb RAM\n", color("reset");
-} else {
-  if (($os_name =~ /(([^64-bit]+))/) && ($memory{TotalPhys} > 3.8)) {
-    print "64-bit OS with more than 3.8Gb RAM\n";
+  if (($os_name =~ /(([^64-bit]+))/) && ($memory{TotalPhys} < 3.8)) {
+    print color("red"), "64-bit OS with less than 3.8Gb RAM\n", color("reset");
   } else {
-    if ($os_name =~ /(([^32-bit]+))/) {
-      print "32-bit OS"
+    if (($os_name =~ /(([^64-bit]+))/) && ($memory{TotalPhys} > 3.8)) {
+      print "64-bit OS with more than 3.8Gb RAM\n";
+    } else {
+      if ($os_name =~ /(([^32-bit]+))/) {
+        print "32-bit OS"
+      }
     }
   }
+  my $drive =  Win32::FsType();
+  print "Drive: $drive\n";
+
+
+  ##############
+  # input
+  #############
+  print "Would you like to add a dummy printer (y/n)? ";
+  my $printer_input = <STDIN>;
+  chomp $printer_input;
+
+  if ($printer_input eq 'n' || $printer_input eq 'N') {
+    print "Will not add a dummy printer";
+  } elsif ($printer_input eq 'y' || $printer_input eq 'Y') {
+      add_printer();
+  } else {
+    print "Please enter a valid y/n answer."
+  }
+
 }
-
-my $drive =  Win32::FsType();
-print "Drive: $drive\n";
-
 
 #################
 #Add printer code
@@ -96,3 +112,7 @@ sub add_printer {
 
 
 my @computer_names = ("Johnson-PC", "Home-PC", "LivingRoom-PC", "Smith-PC", "Anderson-PC", "Johnson-Desktop", "Johnson-Laptop");
+
+
+
+main();
